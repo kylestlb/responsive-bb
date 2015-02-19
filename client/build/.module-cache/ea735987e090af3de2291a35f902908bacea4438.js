2cafd50bdddd3032ed8c1af579e894dd1791f635
kylestlb <kylestlegerbarter@gmail.com>
2015-02-19 14:26:58 -0800

    eschewed in browser JSX transformer in favor of running offline tool and spitting components into client/build

---
+++ client/build/.module-cache/ea735987e090af3de2291a35f902908bacea4438.js
@@ -0,0 +1,15 @@
+
+
+
+var BBApp = React.createClass({displayName: "BBApp",
+	render: function() {
+		return (
+			React.createElement("div", {className: "pure-g wrapper"}, 
+			"Outer App component"
+			)
+		);
+	}
+});
+console.log("pre render");
+React.renderComponent(React.createElement(BBApp, null),document.querySelector('#app'));
+
